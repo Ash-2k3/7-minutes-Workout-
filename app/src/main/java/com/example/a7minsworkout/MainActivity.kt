50a6a8352c692ed4eea724fa2cc8e116e953065d
Ash-2k3 <ash.me.cse@gmail.com>
2022-12-20 15:03:32 +0530

    Start Button and ripple effect added

--- app/src/main/java/com/example/a7minsworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minsworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.a7minsworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val flStartButton : FrameLayout = findViewById(R.id.fl_start_btn)
+        flStartButton.setOnClickListener {
+               Toast.makeText(this,"Starting the exercise" , Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
